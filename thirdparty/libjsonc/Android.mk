affc3048df1651ecc4340ddb55c485bf2a1fa038
insswer <qijiasi001@gmail.com>
2017-04-11 14:02:09 +0800

    delete libjsonc so

--- thirdparty/libjsonc/Android.mk
+++ thirdparty/libjsonc/Android.mk
@@ -2,14 +2,20 @@ LOCAL_PATH := $(my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libjsonc_static
-LOCAL_SRC_FILES := $(call all-c-files-under,src)
-$(info --->$(LOCAL_SRC_FILES))
+LOCAL_SRC_FILES :=	\
+	src/arraylist.c \
+	src/debug.c \
+	src/json_c_version.c \
+	src/json_object.c \
+	src/json_object_iterator.c \
+	src/json_tokener.c \
+	src/json_util.c \
+	src/libjson.c \
+	src/linkhash.c \
+	src/printbuf.c \
+	src/random_seed.c
+
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/
 LOCAL_CFLAGS := -Werror -Wall -Wno-error=deprecated-declarations -Wextra -Wwrite-strings -Wno-unused-parameter -std=gnu99 -D_GNU_SOURCE -D_REENTRANT
 include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libjsonc
-LOCAL_WHOLE_STATIC_LIBRARIES := libjsonc_static
-include $(BUILD_SHARED_LIBRARY)
 
